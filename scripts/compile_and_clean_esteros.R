4237ba9a3c059647b17d836c69a074cd3f028e18
Abram Fleishman <abram@conservationmetrics.com>
2019-04-20 10:19:30 -0700

    new scripts

---
+++ scripts/compile_and_clean_esteros.R
@@ -0,0 +1,273 @@
+# GLM analysis to identify trends in abundance
+#
+# This is an example script to identify terends in abundance for shorebirds in
+# Estero La Cruz, Sonora, MX.  We go through several models and pick a GLM with
+# a negative binomial distribution in the end.
+#
+# Author: Abram Fleishman
+# Date created: 19 Apr 2019
+# Date last modified: 19 Apr 2019
+#
+rm(list=ls())
+
+library(MASS)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(tidyr)
+library(readr)
+library(Kendall)
+library(RColorBrewer)
+library(MuMIn)
+library(patchwork)
+
+options(na.action="na.fail")
+
+### Load and pepare data ###
+if(Sys.info()[6]!="abramfleishman"){
+  # set your paths here Owen
+  cruz<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/LaCruz/LaCruz_Guilds.csv")
+  tast<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Tastiota/Tastiota_guilds.csv")
+  card<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Cardonal/Cardonal_Guilds.csv")
+}else{
+  # paths for abram
+  cruz<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/LaCruz/LaCruz_Guilds.csv")
+  tast<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Tastiota/Tastiota_guilds.csv")
+  card<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Cardonal/Cardonal_Guilds.csv")
+  out_dir<-'/Users/abramfleishman/google_drive/R data Processing/'
+}
+
+asdf<-as.data.frame
+
+# Clean cruz --------------------------------------------------------------
+head(cruz) %>% asdf
+table(is.na(cruz$DateTime))
+cruz %>% names
+unique(cruz$hour)
+cruz$TimeStart %>% sort%>% as.character() %>% unique()
+
+cruz_clean<-cruz %>%
+  mutate(date=ymd(DateTime), # Make date
+         # add a numeric season year
+         year_season=ifelse(monthnum<8,year(date),year(date)+1),
+         # add a day of season
+         day_of_season = as.numeric(date-ymd(paste(year(date),"09","01",sep = "-"))),
+         day_of_season = ifelse(day_of_season <0,day_of_season +365,day_of_season ),
+         #a dd a quadradic term
+         day_of_season2 = day_of_season^2,
+         # add month of season
+         month_of_season = ifelse(monthnum<8, monthnum+3,monthnum-8),
+         # add hour
+         hour=hour(hms(TimeStart)),
+         # fix hour
+         hour=as.numeric(ifelse(hour>500,str_extract(hour,"^[0-9]{2}"),hour)),
+         hour=ifelse(is.na(hour),floor(mean(hour,na.rm=T)),hour),
+         hour=ifelse(hour==2,14,hour),
+         # add missing tides and winds
+         Tide=ifelse(is.na(Tide),"mid-rising",Tide),
+         Wind=ifelse(is.na(Wind),floor(mean(Wind,na.rm=T)),Wind),
+         # fix tide
+         Tide=tolower(Tide),
+         Tide=recode(Tide,
+                     "mid-risng"="mid-rising"),
+         # split tide into height and direction
+         tide_height=str_extract(Tide,"mid|low|high"),
+         tide_height=ifelse(is.na(tide_height),"unk",tide_height),
+         tide_dir=str_extract(Tide,"slack|rising|falling"),
+         tide_dir=ifelse(is.na(tide_dir),"slack",tide_dir),
+         # fix point names
+         PointLoc=gsub("la|santa|site| ","",tolower(PointLoc)),
+         PointLoc=recode(PointLoc,
+                         "crabcoop"="crabcamp",
+                         "oystercamp"="oysterfarm"),
+         Estuary="La Cruz",
+         Cloud=as.numeric(gsub("%","",Cloud))) %>%
+  filter(PointLoc!="restaurant") %>%
+  select(Estuary,Point=PointLoc,date,TimeStart,year_season,month=monthnum,month_of_season,day_of_season,day_of_season2,hour,tide_height,tide_dir,Wind,Cloud,TempF,guild,Species,Count)
+
+names(cruz_clean)<-tolower(names(cruz_clean))
+
+head(cruz_clean) %>% asdf
+
+table(cruz_clean$cloud)
+table(cruz_clean$hour)
+table(cruz_clean$tempf)
+table(cruz_clean$wind)
+table(cruz_clean$point)
+table(cruz_clean$guild)
+
+# oops!  looks like we do not have zeros!  aka when a bird was not seen there is
+# no row for it on that survey!  This is a problem for modeling species but
+# maybe not for modeling guilds.  need to look into it and understand better
+table(cruz_clean$species)
+
+
+# Clean Tastiota ----------------------------------------------------------
+
+
+head(tast) %>% asdf
+table(tast$Estuary)
+table(tast$Tide %>% tolower)
+table(tast$Cloudcover)
+table(tast$TempF )
+table(tast$Precip)
+table(tast$Wind )
+table(tast$PointLoc )
+
+
+tast_clean<-tast %>%
+  mutate(date=ymd(DateTime), # Make date
+         # add a numeric season year
+         monthnum=month(date),
+         year_season=ifelse(monthnum<8,year(date),year(date)+1),
+         # add a day of season
+         day_of_season = as.numeric(date-ymd(paste(year(date),"09","01",sep = "-"))),
+         day_of_season = ifelse(day_of_season <0,day_of_season +365,day_of_season ),
+         #a dd a quadradic term
+         day_of_season2 = day_of_season^2,
+         # add month of season
+         month_of_season = ifelse(monthnum<8, monthnum+3,monthnum-8),
+         # add hour
+         hour=hour(hms(TimeStart)),
+         # fix hour
+         hour=as.numeric(ifelse(hour>500,str_extract(hour,"^[0-9]{2}"),hour)),
+         hour=ifelse(is.na(hour),floor(mean(hour,na.rm=T)),hour),
+         hour=ifelse(hour==2,14,hour),
+         hour=ifelse(hour==1,13,hour),
+         hour=ifelse(hour==5,17,hour),
+         # add missing tides and winds
+         Tide=ifelse(is.na(Tide),"mid-rising",Tide),
+         Wind=ifelse(is.na(Wind),floor(mean(Wind,na.rm=T)),Wind),
+         # fix tide
+         Tide=tolower(Tide),
+         Tide=recode(Tide,
+                     "mid-risng"="mid-rising"),
+         # split tide into height and direction
+         tide_height=str_extract(Tide,"mid|low|high"),
+         tide_height=ifelse(is.na(tide_height),"unk",tide_height),
+         tide_dir=str_extract(Tide,"slack|rising|falling"),
+         tide_dir=ifelse(is.na(tide_dir),"slack",tide_dir),
+         # fix point names
+
+         Estuary=recode(Estuary,
+                        "Tastota"="Tastiota"),
+         Cloudcover=as.numeric(gsub("%","",Cloudcover))) %>%
+  filter(PointLoc!="restaurant") %>%
+  select(Estuary,Point=PointLoc,date,TimeStart,year_season,month=monthnum,month_of_season,day_of_season,day_of_season2,hour,tide_height,tide_dir,Wind,Cloud=Cloudcover,TempF,guild,Species,Count)
+
+names(tast_clean)<-tolower(names(tast_clean))
+
+head(tast_clean) %>% asdf
+tast$TimeStart %>% sort%>% as.character() %>% unique()
+tast_clean$hour %>% table
+tast_clean$tide_height %>% table
+tast_clean$tide_dir %>% table
+
+
+
+# Clean Tastiota ----------------------------------------------------------
+
+
+head(card) %>% asdf
+unique(card$DateTime)
+card$TimeStart %>% sort%>% as.character() %>% unique()
+card$TimeEnd %>% sort%>% as.character() %>% unique()
+
+# remove tastiota and word estero
+table(card$Estuary)
+
+# need to clean up
+table(card$Tide %>% tolower)
+# neeed to remove "%"
+table(card$Cloudcover)
+table(card$TempF )
+
+# what to do with the precip?  remove?
+table(card$Precip)
+# yikes wind need help!
+table(card$Wind )
+
+# need to paredown to the point counts and fix names
+table(card$PointLoc %>% tolower())
+
+
+card_clean<-card %>%
+  mutate(date=ymd(DateTime), # Make date
+         # add a numeric season year
+         monthnum=month(date),
+         year_season=ifelse(monthnum<8,year(date),year(date)+1),
+         # add a day of season
+         day_of_season = as.numeric(date-ymd(paste(year(date),"09","01",sep = "-"))),
+         day_of_season = ifelse(day_of_season <0,day_of_season +365,day_of_season ),
+         #a dd a quadradic term
+         day_of_season2 = day_of_season^2,
+         # add month of season
+         month_of_season = ifelse(monthnum<8, monthnum+3,monthnum-8),
+         # add hour
+         hour=hour(hm(TimeStart)),
+         # fix hour
+         hour=as.numeric(ifelse(hour>500&hour<960,str_extract(hour,"^[0-9]{1}"),hour)),
+         hour=as.numeric(ifelse(hour>999,str_extract(hour,"^[0-9]{2}"),hour)),
+         hour=ifelse(is.na(hour),floor(mean(hour,na.rm=T)),hour),
+         hour=ifelse(hour==2,14,hour),
+         hour=ifelse(hour==1,13,hour),
+         hour=ifelse(hour==5,17,hour),
+         # add missing tides and winds
+         Tide=ifelse(is.na(Tide),"mid-rising",Tide),
+         # wind needs some real fixing
+         Wind=case_when(
+           season%in%c("09-10","10-11","11-12","12-13")&Wind%in%c("light","NW 2") ~ 1,
+           season%in%c("09-10","10-11","11-12","12-13")&Wind%in%c("3","4","5","6","NW 5", "SW 5") ~ 2,
+           season%in%c("09-10","10-11","11-12","12-13")&Wind%in%c("10",'8','7','SW 8') ~ 3,
+           season%in%c("09-10","10-11","11-12","12-13")&Wind%in%c("12","15","15-20") ~ 4,
+           season%in%c("09-10","10-11","11-12","12-13")&Wind%in%c("18") ~ 5,
+           Wind%in%c("1 to 2") ~ 1.5,
+           Wind%in%c("2 to 3") ~ 2.5,
+           is.na(Wind) ~3,
+           TRUE ~0),
+         # fix tide
+         Tide=tolower(Tide),
+         Tide=recode(Tide,
+                     "mid-risng"="mid-rising"),
+         # split tide into height and direction
+         tide_height=str_extract(Tide,"mid|low|high"),
+         tide_height=ifelse(is.na(tide_height),"unk",tide_height),
+         tide_dir=str_extract(Tide,"slack|rising|falling"),
+         tide_dir=ifelse(is.na(tide_dir),"slack",tide_dir),
+         # fix point names
+         PointLoc=gsub(" ","_",tolower(PointLoc)),
+         PointLoc=ifelse(is.na(PointLoc),"travelling",PointLoc),
+         Estuary=gsub("Estero ","",Estuary),
+         Cloudcover=as.numeric(gsub("%","",Cloudcover)),
+         Cloudcover=ifelse(is.na(Cloudcover),round(mean(Cloudcover,na.rm=T)),Cloudcover)) %>%
+  filter(Estuary!="Tastiota") %>%
+  select(Estuary,Point=PointLoc,date,TimeStart,year_season,month=monthnum,month_of_season,day_of_season,day_of_season2,hour,tide_height,tide_dir,Wind,Cloud=Cloudcover,TempF,guild,Species,Count)
+
+names(card_clean)<-tolower(names(card_clean))
+
+
+unique(card_clean$wind)
+head(card_clean) %>% asdf
+card_clean$wind %>%  table(.,useNA = "ifany")
+# card$Wind %>%  table(.,useNA = "ifany")
+
+card_clean$hour %>% table(.,useNA = "ifany")
+card_clean$cloud %>% table(.,useNA = "ifany")
+card_clean$point %>%  table(.,useNA = "ifany")
+card_clean$point %>%  table(.,useNA = "ifany")
+card_clean$tide_height %>%  table(.,useNA = "ifany")
+card_clean$tide_dir %>%  table(.,useNA = "ifany")
+ncol(card_clean)
+ncol(cruz_clean)
+ncol(tast_clean)
+sapply(cruz_clean, function(x)sum(is.na(x)))
+sapply(tast_clean, function(x)sum(is.na(x)))
+
+esteros <- bind_rows(cruz_clean %>% select(-timestart),
+                     card_clean%>% select(-timestart),
+                     tast_clean%>% select(-timestart)) %>%
+  mutate(guild=tolower(guild),
+         point = tolower(point))
+saveRDS(esteros,paste0(out_dir,"compiled_esteros_for_glm_20Apr19.rds"))
+
---
+++ scripts/glm_esteros.R
@@ -0,0 +1,246 @@
+
+# GLM analysis to identify trends in abundance
+#
+# This is an example script to identify terends in abundance for shorebirds in
+# Estero La Cruz, Sonora, MX.  We go through several models and pick a GLM with
+# a negative binomial distribution in the end.
+#
+# Author: Abram Fleishman
+# Date created: 19 Apr 2019
+# Date last modified: 19 Apr 2019
+#
+rm(list=ls())
+
+library(MASS)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(tidyr)
+library(readr)
+library(Kendall)
+library(RColorBrewer)
+library(MuMIn)
+library(patchwork)
+
+options(na.action="na.fail")
+
+### Load and pepare data ###
+if(Sys.info()[6]!="abramfleishman"){
+  # set your paths here Owen
+  cruz<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/LaCruz/LaCruz_Guilds.csv")
+  tast<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Tastiota/Tastiota_guilds.csv")
+  card<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Cardonal/Cardonal_Guilds.csv")
+}else{
+  # paths for abram
+  cruz<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/LaCruz/LaCruz_Guilds.csv")
+  tast<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Tastiota/Tastiota_guilds.csv")
+  card<-read_csv("/Users/abramfleishman/google_drive/R data Processing/CSV/Cardonal/Cardonal_Guilds.csv")
+  out_dir<-'/Users/abramfleishman/google_drive/R data Processing/'
+}
+
+asdf<-as.data.frame
+
+
+# GLM? --------------------------------------------------------------------
+esteros<-readRDS(paste0(out_dir,"compiled_esteros_for_glm_20Apr19.rds"))
+
+# summarize by survey (group all the species)
+sum_by_season_site <- esteros %>%
+  # drop jul, aug, sep for few obs # months 5 and 6 have few birds so might not make sense to include
+  filter(month(date)%in%c(1:6,10:12)) %>%
+  # group by species season and date, this way we can get a total number of each
+  # species seen grouping all the sites.
+  group_by(  estuary,guild,year_season, month_of_season,day_of_season,day_of_season2,tide_dir,tide_height,wind,cloud,point,hour) %>%
+  summarise(total_count=sum(count,na.rm=T)) %>%
+  ungroup  %>% as.data.frame()
+
+# quick checks for missing values and what unique values we have
+table(sum_by_season_site$tide_height,useNA = "ifany")
+table(sum_by_season_site$tide_dir,useNA = "ifany")
+table(sum_by_season_site$hour,useNA = "ifany")
+
+table(sum_by_season_site$point)
+table(sum_by_season_site$point,sum_by_season_site$year_season)
+par(mfrow=c(1,1))
+hist(sum_by_season_site$total_count,breaks = 100)
+
+# plot ?  # need more plots to see what we would predict interms of patterns
+ggplot(sum_by_season_site,aes(x=factor(guild),y = total_count,fill=factor(year_season)))+
+  geom_boxplot()+
+  facet_wrap(~estuary+point,scales="free")
+
+
+
+# GLM (Gaussian) -----
+
+mod_norm<-glm(total_count~year_season+wind+hour+day_of_season+point+tide_height+tide_dir,
+              data=sum_by_season_site)
+
+# calculate risiduals for plotting
+resid_norm<-data.frame(
+  pearsons_residuals=resid(mod_norm,type="pearson"),
+  deviance_residuals=resid(mod_norm,type="deviance"),
+  response_residuals=sum_by_season_site$total_count-predict(mod_norm,type = "response"),
+  predicted=predict(mod_norm,type = "response"),
+  observed=sum_by_season_site$total_count)
+
+# model results
+summary(mod_norm)
+
+# plots for understanding model results
+par(mfrow=c(2,2))
+plot(mod_norm)
+par(mfrow=c(1,1))
+
+# plots for eval of model fit (should be no patters in residuals!)
+ggplot(resid_norm,aes(x=predicted,y=pearsons_residuals))+
+  geom_point()+
+  ggplot(resid_norm,aes(x=predicted,y=deviance_residuals))+
+  geom_point()+
+  ggplot(resid_norm,aes(x=predicted,y=response_residuals))+
+  geom_point()+
+  ggplot(resid_norm,aes(x=predicted,y=observed))+
+  geom_point()
+
+# GLM (Poisson)
+mod_pois<-glm(total_count~year_season+wind+hour+day_of_season+point+tide_height+tide_dir,
+              data=sum_by_season_site,family = "poisson")
+
+resid_pois<-data.frame(
+  pearsons_residuals=resid(mod_pois,type="pearson"),
+  deviance_residuals=resid(mod_pois,type="deviance"),
+  response_residuals=sum_by_season_site$total_count-predict(mod_pois,type = "response"),
+  predicted=predict(mod_pois,type = "response"),
+  observed=sum_by_season_site$total_count)
+
+summary(mod_pois)
+
+# check for overdispersion
+# if not ~=1 than it is over or under dispersed.  if >1 over, if<1 under
+# in this case it is much larger than 1 so we can't fit a poisson model!
+summary(mod_pois)$deviance/summary(mod_pois)$df.residual
+
+par(mfrow=c(2,2))
+plot(mod_pois)
+
+# still patterns
+ggplot(resid_pois,aes(x=predicted,y=pearsons_residuals))+
+  geom_point()+
+  ggplot(resid_pois,aes(x=predicted,y=deviance_residuals))+
+  geom_point()+
+  ggplot(resid_pois,aes(x=predicted,y=response_residuals))+
+  geom_point()+
+  ggplot(resid_pois,aes(x=predicted,y=observed))+
+  geom_point()
+
+# GLM (quasioisson)
+# accounts for over dispersion but can't calculate AIC and trouble for model selection
+# but residuals seem a bit better.  But probably should use negitive binomial distribution
+mod_qpois<-glm(total_count~year_season+hour+day_of_season+point+wind+tide_height+tide_dir,
+               data=sum_by_season_site,family = "quasipoisson")
+
+resid_qpois<-data.frame(
+  pearsons_residuals=resid(mod_qpois,type="pearson"),
+  deviance_residuals=resid(mod_qpois,type="deviance"),
+  response_residuals=sum_by_season_site$total_count-predict(mod_qpois,type = "response"),
+  predicted=predict(mod_qpois,type = "response"),
+  observed=sum_by_season_site$total_count)
+
+summary(mod_qpois)
+par(mfrow=c(2,2))
+plot(mod_qpois)
+
+ggplot(resid_qpois,aes(x=predicted,y=pearsons_residuals))+
+  geom_point()+
+  ggplot(resid_qpois,aes(x=predicted,y=deviance_residuals))+
+  geom_point()+
+  ggplot(resid_qpois,aes(x=predicted,y=response_residuals))+
+  geom_point()+
+  ggplot(resid_qpois,aes(x=predicted,y=observed))+
+  geom_point()
+
+# GLM (Negitive Binomial) -----
+#  remove wind and hour and month
+# there was an arc in the residuals for day_of_season so I added a quadratic (day_of_season2) to account for the fact that in the beginning and end of the season there are low numbers with high numbers in the middle of the season
+
+mod_nb<-glm.nb(total_count~estuary+guild+year_season+day_of_season+day_of_season2+point+tide_height+tide_dir+cloud+wind,
+               data=sum_by_season_site,link = "log")
+
+## full model
+# mod_nb<-glm.nb(total_count~year_season+month_of_season+day_of_season+day_of_season2+hour+wind+point+tide_height+tide_dir,
+#              data=sum_by_season_site,link = "log")
+
+resid_nb<-data.frame(
+  pearsons_residuals=resid(mod_nb,type="pearson"),
+  deviance_residuals=resid(mod_nb,type="deviance"),
+  response_residuals=sum_by_season_site$total_count-predict(mod_nb,type = "response"),
+  predicted=predict(mod_nb,type = "response"),
+  observed=sum_by_season_site$total_count)
+
+summary(mod_nb)
+# should look at pearson's or deviance residuals for eval
+# deviance look ok
+ggplot(resid_nb,aes(x=predicted,y=pearsons_residuals))+
+  geom_point()+
+  ggplot(resid_nb,aes(x=predicted,y=deviance_residuals))+
+  geom_point()+
+  ggplot(resid_nb,aes(x=predicted,y=response_residuals))+
+  geom_point()+
+  ggplot(resid_nb,aes(x=predicted,y=observed))+
+  geom_point()
+
+# normal plots don't look too bad?
+par(mfrow=c(2,2))
+plot(mod_nb)
+
+# component residual plots show the effect of each variable accounting for all
+# the other variables in the model.
+# the blue line is least squares regression line and the pink is a smooth line
+car::crPlots(mod_nb)
+par(mfrow=c(1,1))
+
+# both show a decine here
+car::crPlots(mod_nb,terms = "year_season")
+
+
+sum_by_season_site$pearsons_residuals=resid(mod_nb,type="pearson")
+sum_by_season_site$deviance_residuals=resid(mod_nb,type="deviance")
+sum_by_season_site$response_residuals=sum_by_season_site$total_count-predict(mod_nb,type = "response")
+sum_by_season_site$predicted=predict(mod_nb,type = "response")
+#
+# Plot all the residuals by each variable to confirm there are not any patterns
+#
+# 2016 is a little low
+ggplot(sum_by_season_site,aes(x=factor(year_season),y=deviance_residuals))+geom_boxplot()
+# hour 6 and 15 are a bit different
+ggplot(sum_by_season_site,aes(x=factor(hour),y=deviance_residuals))+geom_boxplot()
+# no pattern now there was before i added the quadratic
+ggplot(sum_by_season_site,aes(x=(day_of_season),y=deviance_residuals))+geom_point()
+# no pattern
+ggplot(sum_by_season_site,aes(x=(day_of_season2),y=deviance_residuals))+geom_point()
+# no pattern
+ggplot(sum_by_season_site,aes(x=factor(point),y=deviance_residuals))+geom_boxplot()
+# hmmI think wind var is still a little wonky
+ggplot(sum_by_season_site,aes(x=factor(wind),y=deviance_residuals))+geom_boxplot()
+# # no pattern
+ggplot(sum_by_season_site,aes(x=factor(tide_height),y=deviance_residuals))+geom_boxplot()
+# no pattern
+ggplot(sum_by_season_site,aes(x=factor(tide_dir),y=deviance_residuals))+geom_boxplot()
+# june ==9 and it is a bit differnt  might drop june?
+ggplot(sum_by_season_site,aes(x=factor(month_of_season),y=deviance_residuals))+geom_boxplot()
+
+# model selection
+mod_nb_dredge<-dredge(mod_nb,extra = "R^2")
+mod_nb_dredge %>% head(10)
+# says we could drop month and wind for sure and maybe hour.
+# Instead of dropping, we can model average and it will essentially do the same thing as removing them
+# but model average would be the best model set, so removal could occur
+#
+# If we do drop month and wind for sure and  hour. there is one model that is WAY better than all the rest. no model averaging needed
+
+# model averaging
+model.avg(mod_nb_dredge) %>% summary
+
+# year results?  30% decline per year?!
+1-exp(-0.37)
